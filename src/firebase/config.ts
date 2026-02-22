131304a313bffa152c6c4f187aca9368ea2eb830
Rahat Ronnel <ronnelrahat@gmail.com>
2026-02-22 15:18:47 +0000

    dear, vercel tell me that need Environment Variables, what can i do dear

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
-// This file is no longer hardcoding the config, but pulling from environment variables.
+// This file pulls the Firebase configuration from environment variables.
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
