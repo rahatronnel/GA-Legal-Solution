67f5c5cbb7a4842f6a35eaf0cc0c82fa3932cbb1
Rahat Ronnel <ronnelrahat@gmail.com>
2026-03-04 17:20:49 +0000

    fix: ignore canvas for pdf rendering

--- next.config.js
+++ next.config.js
@@ -6,6 +6,17 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // ADD THIS SECTION BELOW:
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // This tells the builder to ignore the 'canvas' error
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
